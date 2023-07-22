17111ba41bf7e0febae801eb904fa8a5ed954a40
Abhishek <abhishekdubey013@gmail.com>
2023-07-22 22:19:53 +0530

    l

--- app.js
+++ app.js
@@ -36,60 +36,103 @@ function logAllDataInRedis() {
   });
 }
 
-// Wait for the Redis client to be ready before starting the server
-redisClient.on('ready', () => {
-  const client = new Client();
-  let qrCodeImage = null;
-  const conversations = new Map();
+const client = new Client();
+let qrCodeImage = null;
+const conversations = new Map();
 
-  client.on('qr', (qr) => {
-    qrcode.toDataURL(qr, { errorCorrectionLevel: 'L' }, (err, url) => {
-      if (err) {
-        console.error('QR code generation failed:', err);
-      } else {
-        qrCodeImage = url;
-      }
-    });
-  });
-
-  client.on('ready', () => {
-    console.log('Client is ready');
-  });
-
-  client.initialize();
-
-  const configuration = new Configuration({
-    apiKey: process.env.SECRET_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
-  async function runCompletion(whatsappNumber, message) {
-    // ... (Your existing code for the runCompletion function)
-  }
-
-  client.on('message', (message) => {
-    // ... (Your existing code for handling incoming messages)
-  });
-
-  app.get('/', (req, res) => {
-    if (qrCodeImage) {
-      res.send(`<img src="${qrCodeImage}" alt="QR Code">`);
+client.on('qr', (qr) => {
+  qrcode.toDataURL(qr, { errorCorrectionLevel: 'L' }, (err, url) => {
+    if (err) {
+      console.error('QR code generation failed:', err);
     } else {
-      res.send('QR code image not available');
+      qrCodeImage = url;
     }
   });
+});
 
-  const server = app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+client.on('ready', () => {
+  console.log('Client is ready');
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+  apiKey: process.env.SECRET_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(whatsappNumber, message) {
+  // Get the conversation history and context for the WhatsApp number from Redis
+  redisClient.get(whatsappNumber, async (err, serializedData) => {
+    if (err) {
+      console.error('Error fetching conversation data from Redis:', err);
+      return;
+    }
+
+    // Deserialize the conversation data from JSON
+    const conversation = serializedData
+      ? JSON.parse(serializedData)
+      : { history: [], context: '' };
+
+    // Store the latest message in the history and keep only the last 5 messages
+    conversation.history.push(message);
+    conversation.history = conversation.history.slice(-5);
+
+    const context = conversation.history.join('\n');
+
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: context,
+      max_tokens: 200,
+    });
+
+    // Update the conversation context for the WhatsApp number
+    conversation.context = completion.data.choices[0].text;
+
+    // Serialize the conversation data and store it back in Redis
+    redisClient.set(whatsappNumber, JSON.stringify(conversation));
+
+    return completion.data.choices[0].text;
+  });
+}
+
+client.on('message', (message) => {
+  console.log(message.from, message.body);
+
+  // Get the WhatsApp number from the message
+  const whatsappNumber = message.from;
+
+  // Process the message and send the response
+  runCompletion(whatsappNumber, message.body).then((result) => {
+    message.reply(result);
+  });
+});
+
+app.get('/', (req, res) => {
+  if (qrCodeImage) {
+    res.send(`<img src="${qrCodeImage}" alt="QR Code">`);
+  } else {
+    res.send('QR code image not available');
+  }
+});
+
+// Check if the server is already running on the specified port
+const server = app.listen(port, () => {
+  const address = server.address();
+  if (address) {
+    console.log(`Server is running on http://localhost:${address.port}`);
     // Log all data in Redis when the server is ready
     logAllDataInRedis();
-  });
+  }
 });
 
 // Handle graceful shutdown
 process.on('SIGINT', () => {
   console.log('Closing Redis client and exiting...');
   redisClient.quit(() => {
-    process.exit();
+    // Close the server when the Redis client is properly closed
+    server.close(() => {
+      process.exit();
+    });
   });
 });
