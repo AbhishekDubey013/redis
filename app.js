dadb25c158ceb86b6c4933311c768e88c83bf9cc
Abhishek <abhishekdubey013@gmail.com>
2023-07-26 21:32:47 +0530

    j

--- app.js
+++ app.js
@@ -1,65 +1,87 @@
-const express = require('express');
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode');
-const { Configuration, OpenAIApi } = require('openai');
-const redis = require('redis');
-require('dotenv').config();
+// const express = require('express');
+// const { Client } = require('whatsapp-web.js');
+// const qrcode = require('qrcode');
+// const { Configuration, OpenAIApi } = require('openai');
+// const redis = require('redis');
+// require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 3002;
+// const app = express();
+// const port = process.env.PORT || 3002;
 
-// Redis client to connect to the Redis server using REDIS_URL environment variable
-const redisClient = redis.createClient(process.env.REDIS_URL);
+// // Redis client to connect to the Redis server using REDIS_URL environment variable
+// const redisClient = redis.createClient(process.env.REDIS_URL);
 
-redisClient.on('error', (err) => {
-  console.error('Redis client error:', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('Redis client error:', err);
+// });
 
-const client = new Client();
-let qrCodeImage = null;
+// const client = new Client();
+// let qrCodeImage = null;
 
-client.on('qr', (qr) => {
-  qrcode.toDataURL(qr, { errorCorrectionLevel: 'L' }, (err, url) => {
-    if (err) {
-      console.error('QR code generation failed:', err);
-    } else {
-      qrCodeImage = url;
-    }
+// client.on('qr', (qr) => {
+//   qrcode.toDataURL(qr, { errorCorrectionLevel: 'L' }, (err, url) => {
+//     if (err) {
+//       console.error('QR code generation failed:', err);
+//     } else {
+//       qrCodeImage = url;
+//     }
+//   });
+// });
+
+// client.on('ready', () => {
+//   console.log('Client is ready');
+// });
+
+// const initializeClient = () => {
+//   client.initialize();
+// };
+
+// // Rest of the code...
+
+// // Start the server only after the Redis client is ready
+// redisClient.on('ready', () => {
+//   initializeClient();
+
+//   const server = app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+//     // Log all data in Redis when the server is ready
+//     logAllDataInRedis();
+//   });
+
+//   // Handle graceful shutdown
+//   const shutdown = () => {
+//     console.log('Closing Redis client and exiting...');
+//     redisClient.quit(() => {
+//       console.log('Redis client closed.');
+//       server.close(() => {
+//         console.log('Server closed.');
+//         process.exit();
+//       });
+//     });
+//   };
+
+//   process.on('SIGINT', shutdown);
+//   process.on('SIGTERM', shutdown);
+// });
+
+
+
+import { createClient } from 'redis';
+
+(async () => {
+  // Connect to your internal Redis instance using the REDIS_URL environment variable
+  // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
+  const client = createClient({
+      url: process.env.REDIS_URL
   });
-});
 
-client.on('ready', () => {
-  console.log('Client is ready');
-});
+  client.on('error', (err) => console.log('Redis Client Error', err));
 
-const initializeClient = () => {
-  client.initialize();
-};
+  await client.connect();
 
-// Rest of the code...
+  // Send and retrieve some values
+  await client.set('key', 'node redis');
+  const value = await client.get('key');
 
-// Start the server only after the Redis client is ready
-redisClient.on('ready', () => {
-  initializeClient();
-
-  const server = app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    // Log all data in Redis when the server is ready
-    logAllDataInRedis();
-  });
-
-  // Handle graceful shutdown
-  const shutdown = () => {
-    console.log('Closing Redis client and exiting...');
-    redisClient.quit(() => {
-      console.log('Redis client closed.');
-      server.close(() => {
-        console.log('Server closed.');
-        process.exit();
-      });
-    });
-  };
-
-  process.on('SIGINT', shutdown);
-  process.on('SIGTERM', shutdown);
-});
+  console.log("found value: ", value)
+})();
