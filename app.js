9add0c93e55c8f7c209bfe3ce5cc6c1e82c6ee05
Abhishek <abhishekdubey013@gmail.com>
2023-07-26 20:09:14 +0530

    h

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
 const express = require('express');
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+const { Configuration, OpenAIApi } = require('openai');
 const redis = require('redis');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
-// Redis client to connect to the Redis server using REDIS_URL environment variable (Render provides this variable)
+// Redis client to connect to the Redis server using REDIS_URL environment variable
 const redisClient = redis.createClient(process.env.REDIS_URL);
 
 redisClient.on('error', (err) => {
   console.error('Redis client error:', err);
 });
 
-app.get('/', (req, res) => {
-  // Store a value in Redis
-  redisClient.set('myKey', 'Hello, Redis!', (err, reply) => {
+const client = new Client();
+let qrCodeImage = null;
+
+client.on('qr', (qr) => {
+  qrcode.toDataURL(qr, { errorCorrectionLevel: 'L' }, (err, url) => {
     if (err) {
-      console.error('Error setting value in Redis:', err);
-      res.status(500).send('Error setting value in Redis');
-      return;
+      console.error('QR code generation failed:', err);
+    } else {
+      qrCodeImage = url;
     }
-
-    // Retrieve the value from Redis
-    redisClient.get('myKey', (err, value) => {
-      if (err) {
-        console.error('Error getting value from Redis:', err);
-        res.status(500).send('Error getting value from Redis');
-        return;
-      }
-
-      res.send(`Value from Redis: ${value}`);
-    });
   });
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+client.on('ready', () => {
+  console.log('Client is ready');
 });
 
-// Handle graceful shutdown
-const shutdown = () => {
-  console.log('Closing Redis client and exiting...');
-  redisClient.quit(() => {
-    console.log('Redis client closed.');
-    server.close(() => {
-      console.log('Server closed.');
-      process.exit();
-    });
-  });
+const initializeClient = () => {
+  client.initialize();
 };
 
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+// Rest of the code...
+
+// Start the server only after the Redis client is ready
+redisClient.on('ready', () => {
+  initializeClient();
+
+  const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    // Log all data in Redis when the server is ready
+    logAllDataInRedis();
+  });
+
+  // Handle graceful shutdown
+  const shutdown = () => {
+    console.log('Closing Redis client and exiting...');
+    redisClient.quit(() => {
+      console.log('Redis client closed.');
+      server.close(() => {
+        console.log('Server closed.');
+        process.exit();
+      });
+    });
+  };
+
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
+});
